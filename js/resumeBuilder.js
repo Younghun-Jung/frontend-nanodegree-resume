84a9ab847e32ab3f8800fdb85edbad0f3e2aabf9
Younghun-Jung <youngj6169@gmail.com>
2016-06-30 22:24:27 +0900

    resumeBuilder.js; Add name, role in top

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,1 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+var name = "Jung Younghun";var role = "Web Developer";var formattedName = HTMLheaderName.replace("%data%", name);var formattedRole = HTMLheaderRole.replace("%data%", role);$('#header').prepend(formattedRole);$('#header').prepend(formattedName);
